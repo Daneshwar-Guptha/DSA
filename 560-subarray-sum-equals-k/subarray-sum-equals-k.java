0174ec861f52aa64360090aae0616d358a1edc7f
Daneshwar-Guptha <saiguptha663@gmail.com>
2025-08-19 15:16:01 +0530

    Time: 1602 ms (5.03%) | Memory: 46.4 MB (76.49%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,9 +1,10 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count=0;
-        
+        HashSet<Integer> arr = new HashSet<>();
+      
         for(int i=0;i<nums.length;i++){
-            int sum =0;
+           int sum =0;
             int values =0;
             for(int j=i;j<nums.length;j++){
                 sum+=nums[j];
